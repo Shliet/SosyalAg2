425ce0552153237a4ecf1ba569a821f3dc92353c
Alperen Aydaş <alperenaydasw@gmail.com>
2024-09-22 01:59:12 +0300

    Sosyal Med
    Sosyal Ağlar

---
+++ sosyal med web site/app.js
@@ -0,0 +1,4 @@
+document.getElementById('steam').href = 'https://steamcommunity.com/id/shliet2N/';
+document.getElementById('x').href = 'https://x.com/Shliet2n';
+document.getElementById('instagram').href = 'https://www.instagram.com/alperen.aydas/?next=%2F';
+document.getElementById('spotify').href = 'https://open.spotify.com/user/b8bqyfi4o99ve9lr3glk6l4n7';
